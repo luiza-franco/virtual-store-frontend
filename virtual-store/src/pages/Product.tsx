1ffffd1af9150dd97ce2f2e77eb6648333478c67
Luiza Bade Franco <luizarbfranco@gmail.com>
2023-05-16 21:55:05 -0300

    creation of the shopping cart

--- virtual-store/src/pages/Product.tsx
+++ virtual-store/src/pages/Product.tsx
@@ -5,21 +5,51 @@ import { useEffect, useState } from 'react'
 import styles from './Product.module.css'
 
 interface Product {
+    id: string,
     description: string;
     value: number;
     image: string;
     title: string;
+    quantity: number;
 }
 
 function Product() {
 
-    const [product, setProduct] = useState<Product>({ description: '', value: 0, image: '', title: '' })
+    const [product, setProduct] = useState<Product>({ id: '', description: '', value: 0, image: '', title: '',quantity: 0 })
 
     const { id } = useParams()
 
     async function loadProduct() {
         const response = await axios.get(`http://localhost:3333/products/${id}`)
         setProduct(response.data)
+    }
+
+    function handleAddToCart(product:Product, quantity:number) {
+        const products = localStorage.getItem('products');
+        
+        //pegar os produtos do localstorage OK
+        //verificar no array de produtos se o produto que o usuario quer adicionar já existe no array OK
+        //se existir, adicionar uma quantidade ao produto já existente OK
+        //se não existir adicionar um produto com a quantidade específica OK
+    
+        if (products) {
+            const productsArray = JSON.parse(products) //criação da const que recebe os produtos e depois transforma em objeto
+            let productExisting = false;
+            for(let i = 0; i < productsArray.length; i++) { //percorre todos os produtos do array
+                if(productsArray[i].id === product.id) {    //verificando se o produto adicionado é igual ao existente
+                    productExisting = true;                 //se for igual é true
+                    productsArray[i].quantity += quantity;  //e soma a quantidade adicionada a propriedade quantity
+                }
+            } if (!productExisting) {                       //se for falsa n existe no array
+                product.quantity = quantity;                //a quantity passa a ser a quantity do product
+                productsArray.push(product)                 //o produto é adicionado no final do array
+            }
+            localStorage.setItem('products', JSON.stringify(productsArray)); //array é atualizado(em string) e atualizado localStorage
+        } 
+        else {
+            product.quantity = quantity                     //caso n exista produtos no storage
+            localStorage.setItem('products', JSON.stringify([product])) //é criado um array com um produto e a quantidade dada
+        }
     }
 
     useEffect(() => {
@@ -41,8 +71,8 @@ function Product() {
                 <p>DESCRIÇÃO DO PRODUTO: <br />
                     {product.description}</p>
             </div>
-            <div>
-            <a className={styles.button} href={`/shopping/cart`}><button>ADICIONAR AO CARRINHO</button></a>
+            <div className={styles.button}>
+                <button onClick={() => handleAddToCart(product, 1)}>ADICIONAR AO CARRINHO</button>
             </div>
         </div>
     )
--- virtual-store/src/pages/ShoppingCart.tsx
+++ virtual-store/src/pages/ShoppingCart.tsx
@@ -1,8 +1,36 @@
+import styles from './Product.module.css'
 
 function ShoppingCart() {
 
+    const productsString = localStorage.getItem('products')
+    const products = JSON.parse(productsString ?? '[]'); //o código irá interpretar productsString como uma string vazia '[]' e realizar o parse corretamente
+
     return (
-        <h1>ShoppingCart</h1>
+        <>
+            <ul>
+                {products.map((product: any, index: any) => (
+                    <li key={index}>
+                        <div className={styles.img}>
+                            <img src={product.image} alt="imagem do produto" />
+                        </div>
+                        <div className={styles.title}>
+                            <p></p>{product.title}<p />
+                        </div>
+                        <div className={styles.value}>
+                            <h2>R${product.value}</h2>
+                        </div>
+                        <div className={styles.description}>
+                            <p>DESCRIÇÃO DO PRODUTO: <br />
+                                {product.description}</p>
+                        </div>
+                        <div>
+                            <p>Quantidade<p/>
+                                {product.quantity}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
 
